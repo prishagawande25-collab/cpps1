8fef87de1eb17d01359e5e420541ac4b62978840
Ganisha-Shiv0407 <ganisha.shiv25@pccoepune.org>
2026-05-17 19:01:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -67,5 +67,6 @@ def search():
                            blood_groups=BLOOD_GROUPS, active_tab="search")
  
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=10000)
  
